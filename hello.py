7ff1f2a7aef618f57b57e7d7cd1a2e27855c3b80
Emmanuel Yap Wei'en <2604028@sit.singaporetech.edu.sg>
2026-09-07 15:23:31 +0800

    Re-adding deleted codes

--- hello.py
+++ hello.py
@@ -1,8 +1,29 @@
+#Activity 1: Welcome
 print("=====================")
 print("Welcome here")
 print("My first post!")
 print("=====================")
 
+#Activity 2: Instagram Profile
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+#Activity 3: Followers Update
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
+
+#Activity 4: User Input
 username = input("Enter Username: ")
 age = input("Enter Age: ")
 category = input("Enter Content Category: ")
