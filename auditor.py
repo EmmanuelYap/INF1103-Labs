f3f0102ce5b06d83965be84d43ee2105461dff39
Emmanuel Yap Wei'en <2604028@sit.singaporetech.edu.sg>
2026-09-11 14:25:17 +0800

    auditor.py added

---
+++ auditor.py
@@ -0,0 +1,17 @@
+inventory = 0
+failure = 0
+while True:
+    quantity = input("Enter the quantity of items to add to inventory (or type 'quit' to quit): ")
+    if(quantity.lower() == 'quit'):
+        print(f"Final inventory count: {inventory}")
+        print(f"Number of invalid attempts: {failure}")
+        break
+    elif quantity.isdigit() and int(quantity) >= 0:
+        if(inventory <= 500):
+            inventory += int(quantity)
+        else:
+            print("Inventory limit reached.")
+            break
+    elif not quantity.isdigit():
+        failure += 1
+        print("Invalid input. Please enter a valid number or type 'quit' to exit.")
